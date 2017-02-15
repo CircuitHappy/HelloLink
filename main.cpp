ff68c01ef128dc79af7e2d10823560e3565c012b
Nick Donaldson <ndonald2@gmail.com>
2017-02-14 22:50:58 -0500

    Fixed-width pulses, log phase, reset first

--- main.cpp
+++ main.cpp
@@ -14,8 +14,8 @@ using namespace std;
 
 namespace {
 
-  const double PULSES_PER_BEAT = 1.0;
-  const double RESET_WIDTH = 1.0 / 16.0; // pct of beat
+  const double PULSES_PER_BEAT = 4.0;
+  const double PULSE_LENGTH = 0.015; // seconds
   const double QUANTUM = 4;
 
   // Using WiringPi numbering scheme
@@ -53,7 +53,8 @@ namespace {
       const auto beats = timeline.beatAtTime(time, QUANTUM);
       const auto phase = timeline.phaseAtTime(time, QUANTUM);
       std::cout << "tempo: " << timeline.tempo()
-          << " | " << std::fixed << "beats: " << beats;
+          << " | " << std::fixed << "beats: " << beats
+          << " | " << std::fixed << "phase: " << phase;
       clearLine();
   }
 
@@ -62,19 +63,23 @@ namespace {
           const auto time = state.link.clock().micros();
           auto timeline = state.link.captureAppTimeline();
 
-          const auto beats = timeline.beatAtTime(time, QUANTUM);
-          const auto phase = timeline.phaseAtTime(time, QUANTUM);
+          const double beats = timeline.beatAtTime(time, QUANTUM);
+          const double phase = timeline.phaseAtTime(time, QUANTUM);
+          const double tempo = timeline.tempo();
+          const double secondsPerBeat = 60.0 / tempo;
 
+          // Fractional portion of current beat value
           double intgarbage;
           const auto beatFraction = std::modf(beats * PULSES_PER_BEAT, &intgarbage);
-          const bool gateHigh = (beatFraction <= 0.5);
 
-          digitalWrite(Clock, gateHigh ? HIGH : LOW);
+          // Fractional beat value for which clock should be high
+          const auto highFraction = PULSE_LENGTH / secondsPerBeat;
 
-          // Experiment with PW here
-          const bool resetHigh = (phase <= RESET_WIDTH);
-
+          const bool resetHigh = (phase <= highFraction);
           digitalWrite(Reset, resetHigh ? HIGH : LOW);
+
+          const bool clockHigh = (beatFraction <= highFraction);
+          digitalWrite(Clock, clockHigh ? HIGH : LOW);
 
           std::this_thread::sleep_for(std::chrono::microseconds(250));
       }
